f622e207b190f2d62d9a5a5a0f51b64c678c5e4e
austinbhan <austinbhan@outlook.com>
2022-05-04 17:25:51 -0700

    Submission Build

--- app.js
+++ app.js
@@ -84,9 +84,10 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
+    mushroomsEl.textContent = '';
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
-        
+        mushroomsEl.append(renderMushroom());
     }
 }
 
